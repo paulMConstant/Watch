319e4d59338ed8d264d81102751f8a043cc1de18
Paul Constant <constantpaul@hotmail.com>
2020-05-27 01:26:47 +0200

    Log server into file

--- src/Server/Logger/logger.cpp
+++ src/Server/Logger/logger.cpp
@@ -2,11 +2,18 @@
 
 #include <QTime>
 #include <QTextStream>
+#include <QDir>
 
 void Logger::print(const QString& msg) noexcept
 {
-    auto out = QTextStream(stdout);
-    const auto timeStamp = '[' + QTime::currentTime().toString("hh:mm:ss") + "] ";
-    out << timeStamp << msg;
-    Qt::endl(out);
+    auto outConsole = QTextStream(stdout);
+    const auto timestamp = '[' + QTime::currentTime().toString("hh:mm:ss") + "] ";
+    outConsole << timestamp << msg;
+    Qt::endl(outConsole);
+
+    auto logFile = QFile(QDir::current().path() + "/logs.txt");
+    logFile.open(QIODevice::Append);
+    auto outFile = QTextStream(&logFile);
+    outFile << timestamp << msg;
+    Qt::endl(outFile);
 }
